5216da4eece67b3f417b0d003ecb724e087931c6
miguelvalle05 <miguelvallesin@gmail.com>
2022-09-28 18:33:46 -0500

    multiple

---
+++ MultipleLinearRegression.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+df = pd.read_csv("FuelConsumption.csv")
+
+# take a look at the dataset
+print(df.head())
