4d0bcaad7cbda48ad06c19edcb1a98b82292a098
miguelvalle05 <miguelvallesin@gmail.com>
2022-09-28 14:17:01 -0500

    cylinders vs emission

--- LinearRegression.py
+++ LinearRegression.py
@@ -19,3 +19,21 @@ print(cdf.head(9))
 viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
 viz.hist()
 plt.show()
+
+#Features against the Emission,
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+#Cylinder against the Emission
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Cylinders")
+plt.ylabel("Emission")
+plt.show()
