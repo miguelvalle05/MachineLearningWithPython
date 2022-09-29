7f11d3e58fa8bd32c961d8f719f04d3e6e595ac9
miguelvalle05 <miguelvallesin@gmail.com>
2022-09-29 15:53:13 -0500

    engine

--- MultipleLinearRegression.py
+++ MultipleLinearRegression.py
@@ -7,3 +7,6 @@ df = pd.read_csv("FuelConsumptionCo2.csv")
 
 # take a look at the dataset
 print(df.head())
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
