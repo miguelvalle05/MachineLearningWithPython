3b855bf6337e9641fff959d5799f6608cc5791c7
miguelvalle05 <miguelvallesin@gmail.com>
2022-10-05 12:46:27 -0500

    polynomial

---
+++ PolynomialRegression.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+# take a look at the dataset
+print(df.head())
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+print(cdf.head(9))
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
